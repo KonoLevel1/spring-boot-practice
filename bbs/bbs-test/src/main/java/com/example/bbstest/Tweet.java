b36bf7de0fda911646f18e815877b60a316478b8
Junichi Koyama <koyama@rightcode.co.jp>
2020-07-28 06:47:45 +0900

    書き込み、読み込みが出来るように変更

--- bbs/bbs-test/src/main/java/com/example/bbstest/Tweet.java
+++ bbs/bbs-test/src/main/java/com/example/bbstest/Tweet.java
@@ -1,34 +1,41 @@
 package com.example.bbstest;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+        import javax.persistence.Entity;
+        import javax.persistence.GeneratedValue;
+        import javax.persistence.GenerationType;
+        import javax.persistence.Id;
 
-@Entity
+@Entity // This tells Hibernate to make a table out of this class
 public class Tweet {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+
     private String name;
+
     private String text;
 
-    public Integer getId(){
+    public Integer getId() {
         return id;
     }
-    public void setId(Integer id){
+
+    public void setId(Integer id) {
         this.id = id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getText(){
+
+    public String getText() {
         return text;
     }
-    public void setText(String text){
+
+    public void setText(String text) {
         this.text = text;
     }
 }
--- bbs/bbs-test/src/main/java/com/example/bbstest/TweetRepository.java
+++ bbs/bbs-test/src/main/java/com/example/bbstest/TweetRepository.java
@@ -1,10 +1,8 @@
 package com.example.bbstest;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.bbstest.Tweet;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface TweetRepository extends CrudRepository<Tweet, Integer> {
 
