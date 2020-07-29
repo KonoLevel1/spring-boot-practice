a8223dce61ec383b4e973e5eaae9daf12f256f27
Junichi Koyama <koyama@rightcode.co.jp>
2020-07-29 17:18:02 +0900

    WIP

--- bbs/bbs-test/src/main/java/com/example/bbstest/MainController.java
+++ bbs/bbs-test/src/main/java/com/example/bbstest/MainController.java
@@ -14,6 +14,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+package com.example.jooq.db;
+
+
+import static com.example.jooq.db.Tables.*;
+import static org.jooq.impl.DSL.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.conf.Settings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jooq.db.tables.records.ActorRecord;
+import com.example.studer.sample.tables.Actor;
+import com.example.studer.db.tables.Prefecture;
+
+import javax.annotation.Generated;
+
+import com.example.jooq.db.tables.Actor;
+import com.example.jooq.db.tables.Prefecture;
+
+import javax.annotation.Generated;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @Controller
 public class MainController {
     @Autowired
@@ -21,6 +51,27 @@ public class MainController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
+
+        //
+        String userName = "keiziban";
+        String password = "secret";
+        String url = "jdbc:mysql://localhost:3306/bbs_test?serverTimezone=JST";
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+
+            DSLContext create = using(conn, SQLDialect.MYSQL, settings);
+            create.select(TWEET.NAME, TWEET.TEXT).from(TWEET).orderBy(TWEET.ID.desc().fetch();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "index";
+        //
+
+
+
+
+
+
+
         model.addAttribute("messages", tweetRepository.findAll());
         return "index";
     }
